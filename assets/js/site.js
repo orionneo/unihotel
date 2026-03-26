34ae06a67a1c1899b3403d24b00c8951a7225133
orionneo <44569692+orionneo@users.noreply.github.com>
2026-03-26 15:28:06 -0300

    Delete assets directory

--- assets/js/site.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  var toggle = document.querySelector('[data-nav-toggle]');
-  var menu = document.querySelector('[data-nav-menu]');
-  if (!toggle || !menu) return;
-  toggle.addEventListener('click', function () {
-    menu.classList.toggle('open');
-  });
-})();
